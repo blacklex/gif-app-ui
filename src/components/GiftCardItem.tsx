c171f02d83116d7fd35b85865983ce19ade644b2
blacklex <alexx_gsolis@hotmail.com>
2023-03-05 13:31:13 -0600

    Adding gif api and styles

--- src/components/GiftCardItem.tsx
+++ src/components/GiftCardItem.tsx
@@ -2,11 +2,18 @@ import { FC } from 'react'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface GiftCardItemProps {
-
+  id: string
+  title: string
+  url: string
 }
 
-const GiftCardItem: FC<GiftCardItemProps> = () => {
-  return (<div>Card</div>)
+const GiftCardItem: FC<GiftCardItemProps> = ({ title, url }) => {
+  return (
+    <div className="card">
+      <img src={url} alt={title}></img>
+      <p>{title}</p>
+    </div>
+  )
 }
 
 export default GiftCardItem
--- src/components/GiftGrid.tsx
+++ src/components/GiftGrid.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import GiftCardItem from './GiftCardItem'
 
 interface GiftGridProps {
@@ -6,20 +6,61 @@ interface GiftGridProps {
   onRemoveCategory: (category: string) => void
 }
 
+const getGifts = async (category: string) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=L70bN6g1USOHalSl1fQ55SjChpuuDJgw&q=${category}&limit=10`
+  const response = await fetch(url)
+  const { data } = await response.json()
+
+  const images: { id: string, title: string, url: string } = data.map(
+    (e: {
+      id: string
+      title: string
+      images: { downsized_medium: { url: string } }
+    }) => ({ id: e.id, title: e.title, url: e.images.downsized_medium.url })
+  )
+
+  return images
+}
+
 const GiftGrid: FC<GiftGridProps> = ({ category, onRemoveCategory }) => {
+  const [images, setImages] = useState<
+  Array<{
+    id: string
+    title: string
+    url: string
+  }>
+  >([])
+  useEffect(() => {
+    getGifts(category)
+      .then((data) => {
+        setImages(data)
+      })
+      .catch((e) => {
+        console.log('error')
+      })
+  }, [category])
+
   return (
-    <div>
-      <div style={{ display: 'block' }}>
-      {category}
-      <button
-        onClick={(e) => {
-          onRemoveCategory(category)
-        }}
-      >
-        X
-      </button>
+    <div className="gif-grid">
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        <div style={{ direction: 'initial', flex: 1 }}>
+          <span style={{ fontSize: '3rem' }}>{category}</span>
+        </div>
+        <div style={{ alignSelf: 'center' }}>
+          <button
+            onClick={(e) => {
+              onRemoveCategory(category)
+            }}
+          >
+            X
+          </button>
+        </div>
       </div>
-      <GiftCardItem></GiftCardItem>
+      <div className="card-grid">
+        {images.map((image) => (
+          <GiftCardItem key={image.id} {...image}></GiftCardItem>
+        ))}
+      </div>
     </div>
   )
 }
